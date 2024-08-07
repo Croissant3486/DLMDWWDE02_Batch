24d4dfe21042e91e2c4727190966774c39781b47
Christian Thümmler <c.thuemmler@krz.de>
2024-08-07 11:33:07 +0200

    this kafka script works as batch creator

--- Phase 2/kafka/kafka_producer.py
+++ Phase 2/kafka/kafka_producer.py
@@ -3,9 +3,7 @@ from kafka.admin import NewTopic
 import json
 import pandas as pd
 import logging
-import threading
 import time
-from concurrent.futures import ThreadPoolExecutor
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -17,9 +15,10 @@ def create_kafka_producer():
             producer = KafkaProducer(
                 bootstrap_servers='kafka:9092',
                 value_serializer=lambda v: json.dumps(v).encode('utf-8'),
-                linger_ms=500,  # Increase linger time to allow more batching
                 acks='all',  # Ensure that all replicas acknowledge the message
-                retries=5  # Set the number of retries in case of failure
+                retries=5,  # Set the number of retries in case of failure
+                batch_size=1000000, # 16777216,  # Maximum batch size in bytes (32mb)
+                linger_ms=1000,  # Wait up to 500 ms for more messages to accumulate in the batch
             )
             return producer
         except Exception as e:
@@ -41,7 +40,12 @@ def create_kafka_topic(topic_name, num_partitions=1, replication_factor=1):
             admin_client.create_topics(new_topics=topic_list, validate_only=False)
             logging.info(f"Kafka topic '{topic_name}' created.")
             return
+
         except Exception as e:
+            error_message = str(e)
+            if "exist" in error_message:
+                logging.info(f"Kafka topic '{topic_name}' already exists. Continuing.")
+                return
             logging.error(f"Failed to create Kafka topic: {e}")
             if i < max_retries - 1:
                 logging.info(f"Retrying... ({i + 1}/{max_retries})")
@@ -53,57 +57,84 @@ def create_kafka_topic(topic_name, num_partitions=1, replication_factor=1):
 def send_batch(producer, messages):
     try:
         for message in messages:
+            #logging.info(message)
             producer.send('temperature', message)
-        producer.flush()
-        logging.info("Batch sent successfully")
+        #producer.flush()
+        #logging.info("Batch sent successfully")
     except Exception as e:
         logging.error(f"Error sending batch: {e}")
 
 # Check if the topic exists and create if not
 create_kafka_topic('temperature')
 
-# Read the CSV file
-dataframe = pd.read_csv('/input/german_temperature_data_1996_2021_from_selected_weather_stations.csv')
-
-# Melt the DataFrame to have station_id and temperature as separate columns
-dataframe_melted = dataframe.melt(id_vars=['MESS_DATUM'], var_name='station_id', value_name='temperature').dropna()
-
-# Convert station_id to integer
-dataframe_melted['station_id'] = dataframe_melted['station_id'].astype(int)
-
-# Sort DataFrame by timestamp (no need to sort by station_id now)
-dataframe_melted.sort_values(by=['MESS_DATUM'], inplace=True)
-
-# Initialize variables to manage batching
-batch_size = 2500  # Set your desired batch size
-batch = []
-
 # Create Kafka producer
 producer = create_kafka_producer()
 
-# Initialize ThreadPoolExecutor
-executor = ThreadPoolExecutor(max_workers=5)
+# Read the CSV file
+#dataframe = pd.read_csv()
 
-# Iterate over the DataFrame rows
+#batch_size = 5000
+#batch = []
+
+dataframe = pd.read_csv('/input/german_temperature_data_1996_2021_from_selected_weather_stations.csv', delimiter=",")
+
+dataframe_melted = dataframe.melt(id_vars=['MESS_DATUM'], var_name='station_id', value_name='temperature').dropna()
+
+dataframe_melted['station_id'] = dataframe_melted['station_id'].astype(int)
+
 for index, row in dataframe_melted.iterrows():
     timestamp = row['MESS_DATUM']
     station_id = row['station_id']
-    
     data = {
         'timestamp': timestamp,
         'station_id': station_id,
         'temperature': row['temperature']
     }
-    batch.append(data)
+    #batch.append(data)
+    #if len(batch) >= batch_size:
+    send_batch(producer, data)
+     #   batch = []
 
-    if len(batch) >= batch_size:
-        executor.submit(send_batch, producer, batch)
-        batch = []
+if data:
+     send_batch(producer, data)
 
-# Send any remaining messages in the batch
-if batch:
-    send_batch(producer, batch)
-
-executor.shutdown(wait=True)
 # Close the producer
 producer.close()
+
+# for chunk in pd.read_csv('/input/german_temperature_data_1996_2021_from_selected_weather_stations.csv', delimiter=",", chunksize=batch_size):
+
+#     # Melt the DataFrame to have station_id and temperature as separate columns also drop NaN values
+#     chunk_melted = chunk.melt(id_vars=['MESS_DATUM'], var_name='station_id', value_name='temperature')
+
+#     chunk_melted = chunk_melted.dropna()
+
+#     # Convert station_id to integer
+#     chunk_melted['station_id'] = chunk_melted['station_id'].astype(int)
+
+#     # Sort DataFrame by timestamp (no need to sort by station_id now)
+#     #dataframe_melted.sort_values(by=['MESS_DATUM'], inplace=True)
+
+#     # Initialize variables to manage batching
+#     batch = []
+
+#     # Iterate over the DataFrame rows
+#     for index, row in chunk_melted.iterrows():
+#         timestamp = row['MESS_DATUM']
+#         station_id = row['station_id']
+
+#         data = {
+#             'timestamp': timestamp,
+#             'station_id': station_id,
+#             'temperature': row['temperature']
+#         }
+#         batch.append(data)
+
+#         if len(batch) >= batch_size:
+#             send_batch(producer, batch)
+#             batch = []
+
+# Send any remaining messages in the batch
+# if batch:
+    # send_batch(producer, batch)
+# 
+#executor.shutdown(wait=True)
